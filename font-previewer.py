2373cfad9b1321855923a5514d735c7119c940be
LordUber <mathiasnagy@gmail.com>
2025-04-28 02:54:38 +1000

    working v2 0.2

--- font-previewer.py
+++ font-previewer.py
@@ -7,10 +7,18 @@ from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QSplitter, QListWidget, QListWidgetItem,
     QFileDialog, QWidget, QGridLayout, QScrollArea,
     QPlainTextEdit, QAction, QMessageBox, QToolBar,
-    QLabel, QLineEdit, QCheckBox, QShortcut
+    QLabel, QLineEdit, QCheckBox
 )
-from PyQt5.QtGui import QFontDatabase, QFont, QFontMetrics, QIntValidator, QKeySequence
-from PyQt5.QtCore import Qt, QDir, QStandardPaths
+from PyQt5.QtGui import QFontDatabase, QFont, QFontMetrics, QIntValidator, QKeySequence, QIcon
+from PyQt5.QtCore import Qt, QStandardPaths
+
+# Subclass QPlainTextEdit so Ctrl+C clears the text box instead of copying
+class ClearableTextEdit(QPlainTextEdit):
+    def keyPressEvent(self, event):
+        if event.matches(QKeySequence.Copy):
+            self.clear()
+        else:
+            super().keyPressEvent(event)
 
 # Supported font extensions
 FONT_EXTS = ('.ttf', '.otf')
@@ -21,114 +29,96 @@ class FontPreviewer(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Font Previewer")
+        self.setWindowIcon(QIcon(r"C:\Users\Me\dev\font-previewer\fv_icon.ico"))
 
-        # Size window to 90% of available screen area
+        # Resize to 90% of screen
         screen = QApplication.primaryScreen().availableGeometry()
-        width = int(screen.width() * 0.9)
-        height = int(screen.height() * 0.9)
-        x = screen.x() + (screen.width() - width) // 2
-        y = screen.y() + (screen.height() - height) // 2
-        self.setGeometry(x, y, width, height)
+        w = int(screen.width() * 0.9)
+        h = int(screen.height() * 0.9)
+        x = (screen.width() - w) // 2 + screen.x()
+        y = (screen.height() - h) // 2 + screen.y()
+        self.setGeometry(x, y, w, h)
 
-        # Menu Bar: File and Help
+        # Menu
         fileMenu = self.menuBar().addMenu("File")
         openFolder = QAction("Open Folder…", self)
         openFolder.triggered.connect(self.open_folder)
         fileMenu.addAction(openFolder)
-
         helpMenu = self.menuBar().addMenu("Help")
         aboutAction = QAction("About", self)
         aboutAction.triggered.connect(self.show_about)
         helpMenu.addAction(aboutAction)
 
-        # Toolbar for size/bold/italic
+        # Toolbar
         tb = QToolBar("Font Controls", self)
         self.addToolBar(tb)
-        tb.addWidget(QLabel(" Size:"))
-        self.sizeEdit = QLineEdit("56", self)
+        tb.addWidget(QLabel("Size:"))
+        self.sizeEdit = QLineEdit("46")
         self.sizeEdit.setMaximumWidth(50)
         self.sizeEdit.setValidator(QIntValidator(1, 500, self))
         self.sizeEdit.editingFinished.connect(self.update_font_settings)
         tb.addWidget(self.sizeEdit)
-        self.boldCheck = QCheckBox("Bold", self)
+        self.boldCheck = QCheckBox("Bold")
         self.boldCheck.stateChanged.connect(self.update_font_settings)
         tb.addWidget(self.boldCheck)
-        self.italicCheck = QCheckBox("Italic", self)
+        self.italicCheck = QCheckBox("Italic")
         self.italicCheck.stateChanged.connect(self.update_font_settings)
         tb.addWidget(self.italicCheck)
+        tb.addWidget(QLabel(" Alt code:"))
+        self.altCodeEdit = QLineEdit()
+        self.altCodeEdit.setMaximumWidth(50)
+        self.altCodeEdit.setValidator(QIntValidator(0, 1114111, self))
+        self.altCodeEdit.editingFinished.connect(self.insert_alt_code_symbol)
+        tb.addWidget(self.altCodeEdit)
 
-        # Main splitter: left file list, right vertical panels
+        # Splitters
         outer = QSplitter(Qt.Horizontal)
         self.setCentralWidget(outer)
-
-        # Left: file list
         self.fileList = QListWidget()
-        self.fileList.setMouseTracking(True)
         self.fileList.itemClicked.connect(self.on_file_clicked)
         outer.addWidget(self.fileList)
 
-        # Right: preview and text
         rightSplit = QSplitter(Qt.Vertical)
         outer.addWidget(rightSplit)
         outer.setStretchFactor(1, 3)
 
-        # Top: glyph preview
+        # Preview area
         self.previewWidget = QWidget()
-        # ensure white background
-        self.previewWidget.setStyleSheet("background-color: white;")
-        self.previewLayout = QGridLayout()
-        self.previewWidget.setLayout(self.previewLayout)
+        self.previewWidget.setStyleSheet("background-color:white;")
+        self.previewLayout = QGridLayout(self.previewWidget)
         self.previewScroll = QScrollArea()
-        # set scroll viewport white too
-        self.previewScroll.setStyleSheet("background-color: white;")
+        self.previewScroll.setStyleSheet("background-color:white;")
         self.previewScroll.setWidgetResizable(True)
         self.previewScroll.setWidget(self.previewWidget)
         rightSplit.addWidget(self.previewScroll)
 
-        # Bottom: text entry
-        self.textEdit = QPlainTextEdit()
+        # Text entry
+        self.textEdit = ClearableTextEdit()
         self.textEdit.setPlaceholderText("Type here…")
         self.textEdit.textChanged.connect(self.on_text_changed)
         rightSplit.addWidget(self.textEdit)
 
-        # Ctrl+C clears text
-        clear_sc = QShortcut(QKeySequence("Ctrl+C"), self.textEdit)
-        clear_sc.activated.connect(self.textEdit.clear)
-
-        # Split 50/50
         rightSplit.setStretchFactor(0, 1)
         rightSplit.setStretchFactor(1, 1)
-        rightSplit.setSizes([height//2, height//2])
+        rightSplit.setSizes([h//2, h//2])
 
-        # Default folder: Documents/fonts or Documents
+        # Default folder
         docs = QStandardPaths.writableLocation(QStandardPaths.DocumentsLocation)
         fonts_dir = os.path.join(docs, 'fonts')
         self.currentFolder = fonts_dir if os.path.isdir(fonts_dir) else docs
 
-        # Initial font state
         self.currentFont = QApplication.font()
-        self.currentFontSize = 56
+        self.currentFontSize = 44
 
-        # Load folder contents and focus text entry
         self.load_folder(self.currentFolder)
         self.textEdit.setFocus()
 
     def show_about(self):
-        # Custom about dialog: centered content and manual padding
-        mb = QMessageBox(self)
-        mb.setWindowTitle("About Font Previewer")
-        mb.setTextFormat(Qt.RichText)
-        mb.setText(
-            '<div align="center">'
-            'Developed by <a href="http://www.mathiasnagy.com">Mathias Nagy</a>&nbsp;&nbsp;&nbsp;<br>'
-            '<br>'
-            '&nbsp;Coded by ChatGPT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>'
-            '<br>'
-            '&nbsp;23 April 2025&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
-            '</div>'
-        )
-        mb.setStandardButtons(QMessageBox.Ok)
-        mb.exec_()
+        QMessageBox.about(self, "About Font Previewer",
+                          '<div align="center">'
+                          '   <a href="https://github.com/LordUber">(c) Mathias Nagy</a><br>'
+                          'Released under MIT license<br>'
+                          '23 April 2025 </div>')
 
     def open_folder(self):
         d = QFileDialog.getExistingDirectory(self, "Select Font Folder", self.currentFolder)
@@ -139,15 +129,14 @@ class FontPreviewer(QMainWindow):
         self.currentFolder = folder
         self.fileList.clear()
         for fn in sorted(os.listdir(folder), key=str.lower):
-            low = fn.lower()
-            if low.endswith(FONT_EXTS) or low.endswith(ARCHIVE_EXT):
+            if fn.lower().endswith(FONT_EXTS) or fn.lower().endswith(ARCHIVE_EXT):
                 item = QListWidgetItem(fn)
                 full = os.path.join(folder, fn)
-                if low.endswith(ARCHIVE_EXT):
+                if fn.lower().endswith(ARCHIVE_EXT):
                     try:
                         with zipfile.ZipFile(full) as z:
-                            contains = any(n.lower().endswith(FONT_EXTS) for n in z.namelist())
-                        item.setToolTip(full if contains else "no fonts in here")
+                            has = any(n.lower().endswith(FONT_EXTS) for n in z.namelist())
+                        item.setToolTip(full if has else "no fonts in here")
                     except zipfile.BadZipFile:
                         item.setToolTip("invalid zip")
                 else:
@@ -156,88 +145,124 @@ class FontPreviewer(QMainWindow):
 
     def on_file_clicked(self, item):
         path = os.path.join(self.currentFolder, item.text())
-        low = path.lower()
-        if low.endswith(ARCHIVE_EXT):
+        if path.lower().endswith(ARCHIVE_EXT):
             try:
                 with zipfile.ZipFile(path) as z:
-                    candidates = [n for n in z.namelist() if n.lower().endswith(FONT_EXTS)]
-                    if not candidates:
+                    fonts = [n for n in z.namelist() if n.lower().endswith(FONT_EXTS)]
+                    if not fonts:
                         QMessageBox.information(self, "No Fonts", "no fonts in here")
                         return
-                    data = z.read(candidates[0])
+                    data = z.read(fonts[0])
             except zipfile.BadZipFile:
                 QMessageBox.warning(self, "Error", "Invalid ZIP archive")
                 return
-            ext = os.path.splitext(candidates[0])[1]
-            tmp = tempfile.NamedTemporaryFile(delete=False, suffix=ext)
-            tmp.write(data)
-            tmp.close()
-            font_path = tmp.name
-        elif low.endswith(FONT_EXTS):
-            font_path = path
-        else:
-            return
-        self.load_font(font_path)
+            tmp = tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(fonts[0])[1])
+            tmp.write(data); tmp.close()
+            path = tmp.name
+        self.load_font(path)
 
     def load_font(self, path):
+        # Clear preview
         while self.previewLayout.count():
             w = self.previewLayout.takeAt(0).widget()
-            if w:
-                w.deleteLater()
-        font_id = QFontDatabase.addApplicationFont(path)
-        if font_id < 0:
-            QMessageBox.warning(self, "Error", "Failed to load font.")
-            return
-        fams = QFontDatabase.applicationFontFamilies(font_id)
+            if w: w.deleteLater()
+        fid = QFontDatabase.addApplicationFont(path)
+        if fid < 0:
+            QMessageBox.warning(self, "Error", "Failed to load font."); return
+        fams = QFontDatabase.applicationFontFamilies(fid)
         if not fams:
-            QMessageBox.warning(self, "Error", "No font family found.")
-            return
-        fam = fams[0]
-        self.currentFont = QFont(fam, self.currentFontSize)
+            QMessageBox.warning(self, "Error", "No font family found."); return
+        self.currentFont = QFont(fams[0], self.currentFontSize)
         self.apply_style_flags()
         self.render_preview()
         self.textEdit.setFont(self.currentFont)
         self.textEdit.setFocus()
 
+    def insert_alt_code_symbol(self):
+        code = self.altCodeEdit.text()
+        if code:
+            try:
+                ch = chr(int(code))
+                self.textEdit.insertPlainText(ch)
+            except:
+                QMessageBox.warning(self, "Invalid Code", f"Invalid alt code: {code}")
+
     def render_preview(self):
+        # Clear any existing widgets
         while self.previewLayout.count():
             w = self.previewLayout.takeAt(0).widget()
             if w:
                 w.deleteLater()
+
         fm = QFontMetrics(self.currentFont)
         default_font = QApplication.font()
-        supported = [chr(c) for c in ASCII_RANGE if fm.inFont(chr(c))]
-        if not supported:
-            self.previewLayout.addWidget(QLabel("no characters in file"), 0, 0)
-            return
+        ref_font = QFont(default_font.family(), 14)
         cols = 10
-        for i, ch in enumerate(supported):
-            r = (i // cols) * 2
-            c = i % cols
-            lbl_sys = QLabel(ch)
-            lbl_sys.setFont(default_font)
-            self.previewLayout.addWidget(lbl_sys, r, c)
-            lbl_fnt = QLabel(ch)
-            lbl_fnt.setFont(self.currentFont)
-            self.previewLayout.addWidget(lbl_fnt, r+1, c)
+
+        # 1) Core ASCII characters
+        for idx, code in enumerate(ASCII_RANGE):
+            ch = chr(code)
+            r = (idx // cols) * 2
+            c = idx % cols
+
+            # Reference label with code
+            lbl_ref = QLabel(f"{ch} ({code})")
+            lbl_ref.setFont(ref_font)
+            self.previewLayout.addWidget(
+                lbl_ref, r, c, 1, 1, Qt.AlignBottom | Qt.AlignHCenter
+            )
+
+            # Font glyph only if supported
+            if fm.inFontUcs4(code):
+                lbl_gly = QLabel(ch)
+                lbl_gly.setFont(self.currentFont)
+            else:
+                lbl_gly = QLabel("")
+            self.previewLayout.addWidget(
+                lbl_gly, r+1, c, 1, 1, Qt.AlignTop | Qt.AlignHCenter
+            )
+
+        # 2) “Other Unicode” separator
+        sep_row = ((len(ASCII_RANGE) + cols - 1) // cols) * 2
+        sep = QLabel("Other Unicode")
+        sep.setAlignment(Qt.AlignCenter)
+        self.previewLayout.addWidget(sep, sep_row, 0, 1, cols)
+
+        # 3) Extended block U+007F–U+0400 (127–1024)
+        for j, code in enumerate(range(127, 1025)):
+            ch = chr(code)
+            r = sep_row + 1 + (j // cols) * 2
+            c = j % cols
+
+            lbl_ref_o = QLabel(f"{ch} ({code})")
+            lbl_ref_o.setFont(ref_font)
+            self.previewLayout.addWidget(
+                lbl_ref_o, r, c, 1, 1, Qt.AlignBottom | Qt.AlignHCenter
+            )
+
+            if fm.inFontUcs4(code):
+                lbl_gly_o = QLabel(ch)
+                lbl_gly_o.setFont(self.currentFont)
+            else:
+                lbl_gly_o = QLabel("")
+            self.previewLayout.addWidget(
+                lbl_gly_o, r+1, c, 1, 1, Qt.AlignTop | Qt.AlignHCenter
+            )
 
     def update_font_settings(self):
         try:
             sz = int(self.sizeEdit.text())
-            if sz > 0:
-                self.currentFontSize = sz
-        except ValueError:
+            if sz > 0: self.currentFontSize = sz
+        except:
             pass
         self.apply_style_flags()
-        if self.currentFont:
-            self.currentFont.setPointSize(self.currentFontSize)
-            self.render_preview()
-            self.textEdit.setFont(self.currentFont)
+        self.currentFont.setPointSize(self.currentFontSize)
+        self.render_preview()
+        self.textEdit.setFont(self.currentFont)
 
     def apply_style_flags(self):
-        if self.currentFont:
-            self.currentFont.setBold(self.boldCheck.isChecked())
-            self.currentFont.setItalic(self.italicCheck.isChecked())
+        self.currentFont.setBold(self.boldCheck.isChecked())
+        self.currentFont.setItalic(self.italicCheck.isChecked())
 
     def on_text_changed(self):
         if self.textEdit.verticalScrollBar().maximum() > 0:
